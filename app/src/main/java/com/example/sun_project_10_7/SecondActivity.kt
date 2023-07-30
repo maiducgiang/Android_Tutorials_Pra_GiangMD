a114c524b7923bfbca4ca67e6901e70ce48c1eb6
Mabook Pro <www.maiducgiang@gmail.com>
2023-07-30 21:46:28 +0700

    final recycleview kotlin

--- app/src/main/java/com/example/sun_project_10_7/SecondActivity.kt
+++ app/src/main/java/com/example/sun_project_10_7/SecondActivity.kt
@@ -2,10 +2,17 @@ package com.example.sun_project_10_7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sun_project_10_7.data.Datasource
+import com.example.sun_project_10_7.data.adapter.ItemAdapter
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
