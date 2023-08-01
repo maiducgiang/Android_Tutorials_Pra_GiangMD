851f732caed13568f122d4139138746301b88d8d
Mabook Pro <>
2023-08-01 16:42:10 +0700

    add menu in appbar, add datepicker

--- app/src/main/java/com/example/sun_project/MainActivity.kt
+++ app/src/main/java/com/example/sun_project/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sun_project
 
+import android.app.DatePickerDialog
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
@@ -18,12 +19,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import com.example.sun_project.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
-
+    private var mainMenu: Menu? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,23 +33,56 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         var intent: Intent = Intent(Intent.ACTION_SEND)
 
-        binding.nextPageCallApi.setOnClickListener{
+        binding.nextPageCallApi.setOnClickListener {
             val intent = Intent(this, ThreeActivity::class.java)
             startActivity(intent)
         }
-        binding.intentFilterSend.setOnClickListener{
+        binding.intentFilterSend.setOnClickListener {
             intent.type = "text/plain"
             intent.putExtra(Intent.EXTRA_EMAIL, "maiducgiang@gmail.com")
             intent.putExtra(Intent.EXTRA_SUBJECT, "send data")
 
-            startActivity(Intent.createChooser(intent,"chon email"))
+            startActivity(Intent.createChooser(intent, "chon email"))
         }
-
+        binding.dialogPicker.setOnClickListener{
+            showDatePickerDialog()
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        Toast.makeText(this,"Chào mừng bạn quay trở lại",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Chào mừng bạn quay trở lại", Toast.LENGTH_SHORT).show()
     }
 
+    private fun showDatePickerDialog() {
+        // Get the current date for initialization of the Date Picker dialog
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Create and show the Date Picker dialog
+        val datePickerDialog = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+
+                println(year + monthOfYear+ dayOfMonth)
+
+            },
+            year,
+            month,
+            day
+        )
+        datePickerDialog.show()
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        mainMenu = menu
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+
+    }
 }
